1e782f230df664ac0b52ba28effc86d30b86d4f5
fhanifatun29 <62748344+fhanifatun29@users.noreply.github.com>
2020-06-28 09:17:49 +0700

    Add files via upload

---
+++ prosess.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TUGAS FORM</title>
+</head>
+<body>
+	<center><h1>FORMULIR PENDAFTARAN</h1></center>
+	
+	NAMA LENGKAP  : <?php echo $_POST["Nama_lengkap"];?><br></br>
+	TEMPAT LAHIR  : <?php echo $_POST["tempat_lahir"];?><br></br>
+	TANGGAL LAHIR : <?php echo $_POST["tgl_lahir"];?><br></br>
+	ALAMAT        :  <?php echo $_POST["Alamat"];?><br></br>
+	EMAIL 		  : <?php echo $_POST["Email"];?><br></br>
+	AGAMA 		  : <?php echo $_POST["Agama"]; ?><br></br>
+
+
+
+</body>
+</html>
